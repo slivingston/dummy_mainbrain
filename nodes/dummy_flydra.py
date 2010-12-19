c52bbb5a50bd2719bd49a84bb63c033fcfce9be2
Scott Livingston <slivingston@caltech.edu>
2010-12-18 22:05:58 -0800

    update manifest file; fixed curious import bug; fix_nobj option no disallows births, deaths.

--- nodes/dummy_flydra.py
+++ nodes/dummy_flydra.py
@@ -11,7 +11,7 @@ modifications by Scott Livingston.
 """
 
 
-import roslib; roslib.load_manifest('ros_flydra')
+import roslib; roslib.load_manifest('dummy_mainbrain')
 import rospy
 import ros_flydra.msg as msgs
 import geometry_msgs.msg as geometry_msgs
@@ -25,12 +25,17 @@ verbose_mode = False
 
 class DummyMainbrain:
 
-    def __init__(self, nobjects=5, latency=0.05, birth_rate=0.02, death_rate=0.02,
+    def __init__(self, nobjects=5, latency=0.05,
+                 birth_rate=0.02, death_rate=0.02,
                  fix_nobj=False):
         self.max_num_objects = nobjects
         self.latency = latency
-        self.prob_birth = birth_rate
-        self.prob_death = death_rate
+        if fix_nobj:
+            self.prob_birth = 0.
+            self.prob_death = 0.
+        else:
+            self.prob_birth = birth_rate
+            self.prob_death = death_rate
         
         self.newest_object = 0
         self.framenumber = 0
@@ -41,14 +46,14 @@ class DummyMainbrain:
         else:
             init_objects = np.random.randint(0,self.max_num_objects)
         if verbose_mode:
-            print "Generating %d flies..." % init_objects
+            print "Generating %d random points..." % init_objects
             print "Latency set to %d" % self.latency
             print "Probability of birth is %.4f" % self.prob_birth
             print "Probability of death is %.4f" % self.prob_death
-        self.objects = []
+        self.point_list = []
         for i in range(init_objects):
             self.newest_object = self.newest_object + 1
-            self.objects.append( DummyObject(self.newest_object) )
+            self.point_list.append( DummyPoint(self.newest_object) )
         
         # ros stuff
         self.pub = rospy.Publisher("flydra_mainbrain_super_packets", msgs.flydra_mainbrain_super_packet)
@@ -63,20 +68,25 @@ class DummyMainbrain:
         time.sleep(self.latency)
         
         birth_check = np.random.random()
-        if birth_check < self.prob_birth:
-            if len(self.objects) < self.max_num_objects:
-                self.newest_object = self.newest_object+1
-                self.objects.append(DummyObject(self.newest_object))
+        if birth_check < self.prob_birth and len(self.point_list) < self.max_num_objects:
+            if verbose_mode:
+                print "\nGenerating new object..."
+            self.newest_object = self.newest_object+1
+            self.point_list.append(DummyPoint(self.newest_object))
+            if verbose_mode:
+                print "Now %d total objects." % len(self.point_list)
 
         death_check = np.random.random()
-        if death_check < self.prob_death:
-            if len(self.objects) > 1:
-                del self.objects [np.random.randint(0,len(self.objects))]
+        if death_check < self.prob_death and len(self.point_list) > 1:
+            ind_to_kill = np.random.randint(0,len(self.point_list))
+            if verbose_mode:
+                print "\nKilling object %d (at index %d)..." % (self.point_list[ind_to_kill].get_id(), ind_to_kill)
+            del self.point_list[ind_to_kill]
 
         # package with mainbrain message format
         flydra_objects = []
-        for i in range(len(self.objects)):
-            obj_id, position, velocity, posvel_covariance_diagonal = self.objects[i].get_state()
+        for i in range(len(self.point_list)):
+            obj_id, position, velocity, posvel_covariance_diagonal = self.point_list[i].get_state()
             flydra_object = msgs.flydra_object(obj_id, geometry_msgs.Point(position[0], position[1], position[2]), geometry_msgs.Vector3(velocity[0], velocity[1], velocity[2]), posvel_covariance_diagonal)
             flydra_objects.append(flydra_object)
         
@@ -94,9 +104,9 @@ class DummyMainbrain:
         while not rospy.is_shutdown():
             time.sleep(self.latency)
             self.get_objects()
-        
-class DummyObject:
 
+        
+class DummyPoint:
     def __init__(self, obj_id):
         # random start position values
         self.x0 = np.random.randn()
@@ -112,10 +122,15 @@ class DummyObject:
         self.obj_id = obj_id
 
         if verbose_mode:
-            print "DummyObject %d initialized with\n\t(%.4f, %.4f, %.4f) pose,"\
+            print "DummyPoint %d initialized with\n\t(%.4f, %.4f, %.4f) pose,"\
                 "\n\t(%.4f, %.4f, %.4f) amplitudes." % (self.obj_id,
                                                         self.x0, self.y0, self.z0,
                                                         self.xamp, self.yamp, self.zamp)
+
+    def get_id(self):
+        """Return this object's ID number.
+"""
+        return self.obj_id
 
     def get_state(self):
         theta = time.time() % (2*np.pi)
@@ -145,7 +160,7 @@ if __name__ == '__main__':
     parser.add_option("--death-rate", type="float", dest="death_rate", default=0.02,
                       help="probability that a new object dies")
     parser.add_option("-f", "--fix-count", action="store_true", dest="fix_nobj", default=False,
-                      help="use fixed number of objects (rather than random).")
+                      help="use fixed number of objects (rather than random; note that birth/death rates are ignored in this case).")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose_mode", default=False,
                       help="verbose mode")
     (options, args) = parser.parse_args()
